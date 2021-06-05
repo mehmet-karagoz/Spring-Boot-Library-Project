67f821e33a876e526981ff3c2342d7900b5bfb51
mehmet-karagoz <atleownhem@gmail.com>
2021-06-05 16:54:10 +0300

    added one more method

--- src/main/java/com/mehmetkaragoz/libraryProject/repository/BookRepository.java
+++ src/main/java/com/mehmetkaragoz/libraryProject/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package com.mehmetkaragoz.libraryProject.repository;
 
-import com.mehmetkaragoz.libraryProject.model.Book;
+import java.util.List;
 
+//import java.util.Optional;
+import com.mehmetkaragoz.libraryProject.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
-    
+
+    //this is not working bc title is not unique
+    // @Query("SELECT b FROM Book b WHERE b.title = ?1")
+    // Optional<Book> findBookByTitle(String title);
+
+    @Query("SELECT b FROM Book b WHERE b.title = ?1")
+    List<Book> findBookByTitle(String title);
 }
