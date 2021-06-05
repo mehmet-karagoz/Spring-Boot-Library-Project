2ef432b2d3589ba13ae7b3bb2822512e80bef894
mehmet-karagoz <atleownhem@gmail.com>
2021-06-05 16:21:50 +0300

    to test some code added

--- src/main/java/com/mehmetkaragoz/libraryProject/LibraryProjectApplication.java
+++ src/main/java/com/mehmetkaragoz/libraryProject/LibraryProjectApplication.java
@@ -1,13 +1,38 @@
 package com.mehmetkaragoz.libraryProject;
 
+import java.util.List;
+
+import com.mehmetkaragoz.libraryProject.model.Book;
+import com.mehmetkaragoz.libraryProject.repository.BookRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryProjectApplication.class, args);
+
+		
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(BookRepository bookrepo) {
+		return args -> {
+			Book book1 = new Book("Ataturk", "Ilber Ortayli", 35);
+
+			System.out.println("Adding book1...");
+			bookrepo.save(book1);
+			System.out.println("Number of book: " + bookrepo.count());
+
+
+			System.out.println("Select all books...");
+			List<Book> listBooks = bookrepo.findAll();
+
+			listBooks.forEach(System.out::println);
+		};
+	}
 }
