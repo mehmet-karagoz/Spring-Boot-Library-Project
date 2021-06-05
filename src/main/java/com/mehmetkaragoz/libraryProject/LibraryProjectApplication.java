7e78fa8b0eb425d137c6af4b9c4e23d4118350ab
mehmet-karagoz <atleownhem@gmail.com>
2021-06-05 16:53:30 +0300

    adding more code for test to work

--- src/main/java/com/mehmetkaragoz/libraryProject/LibraryProjectApplication.java
+++ src/main/java/com/mehmetkaragoz/libraryProject/LibraryProjectApplication.java
@@ -23,16 +23,24 @@ public class LibraryProjectApplication {
 	CommandLineRunner commandLineRunner(BookRepository bookrepo) {
 		return args -> {
 			Book book1 = new Book("Ataturk", "Ilber Ortayli", 35);
+			Book book2 = new Book("Mustafa", "Ilber Ortayli", 30);
+			Book book3 = new Book("Kemal", "Ilber Ortayli", 25);
+			Book book4 = new Book("Pasa", "Ilber Ortayli", 15);
+			Book book5 = new Book("Mustafa", "George", 50);
 
-			System.out.println("Adding book1...");
-			bookrepo.save(book1);
+			//System.out.println("Adding book1...");
+			//bookrepo.save(book1);
+			bookrepo.saveAll(List.of(book1, book2, book3, book4, book5));
 			System.out.println("Number of book: " + bookrepo.count());
-
 
 			System.out.println("Select all books...");
 			List<Book> listBooks = bookrepo.findAll();
-
 			listBooks.forEach(System.out::println);
+
+			bookrepo.findById(2L).ifPresentOrElse(System.out::println, 
+			() -> System.out.println("Can not find any book with 2 id"));
+
+			bookrepo.findBookByTitle("Mustafa").forEach(System.out::println);
 		};
 	}
 }
