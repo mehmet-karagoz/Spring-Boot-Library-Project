3f19a18573569158d9e30bf53cdfc132fe73f94e
mehmet-karagoz <atleownhem@gmail.com>
2021-06-05 14:49:24 +0300

    initial commit

---
+++ src/test/java/com/mehmetkaragoz/libraryProject/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetkaragoz.libraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
